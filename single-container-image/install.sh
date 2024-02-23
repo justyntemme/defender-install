5a6d024e170da49b2e907b0236dae9d1838b5789
justyn temme <justyntemme@gmail.com>
2024-02-23 12:52:16 -0500

    install script for defender

---
+++ single-container-image/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker before running this script."
+    exit 1
+fi
+
+loginResponse=$(curl -s -X POST "${TL_URL}/api/v1/authenticate" \
+    -H 'accept: application/json; charset=UTF-8' \
+    -H 'content-type: application/json' \
+    -d '{"username": "'"$PC_IDENTITY"'", "password": "'"$PC_SECRET"'"}')
+
+
+# Parse the response and extract the token
+token=$(echo "$loginResponse" | jq -r '.token')
+
+if [ "$token" == "null" ] || [ -z "$token" ]; then
+    echo "Failed to obtain Token"
+    exit 1
+fi
+
+export TL_TOKEN="$token"
+echo "Token obtained: "
+
+# Query defender.sh endpoint and save the script
+defenderScript=$(curl -sSL --header "authorization: Bearer $TL_TOKEN" -X POST "${TL_URL}/api/v1/scripts/defender.sh")
+echo "$defenderScript" > defender-install.sh
+
+chmod +x defender-install.sh
+./defender-install.sh
+
+# Check the exit status of defender-install.sh
+exit_status=$?
+
+if [ $exit_status -eq 0 ]; then
+    echo "Defender installation completed successfully."
+else
+    echo "Defender installation failed with exit code $exit_status."
+fi
+
+exit $exit_status
+
